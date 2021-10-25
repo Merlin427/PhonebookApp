80f0fcdc94ea83c66f9abe26a311a4b78db7d842
DanievR <danie@jager.co.za>
2021-10-25 20:22:55 +0200

    Fixed repo bug

--- PhonebookApi/PhonebookApi/Repositories/IPhonebookRepo.cs
+++ PhonebookApi/PhonebookApi/Repositories/IPhonebookRepo.cs
@@ -50,6 +50,7 @@ namespace PhonebookApi.Repositories
                 {
                     PhonebookId = entries.PhonebookId,
                     ContactName = entries.ContactName,
+                    PhoneNumber = entries.PhoneNumber,
                     Id = entries.Id
                 })
             };
